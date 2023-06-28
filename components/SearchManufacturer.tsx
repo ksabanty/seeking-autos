5c52fcee4b64d19a64c3e9ca0d0b06c9ab44be0b
Kerry Sabanty <kgsabanty@gmail.com>
2023-06-28 18:41:37 -0400

    finishing search dropdown

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -5,7 +5,6 @@ import { SearchManufacturerProps } from "@/types"
 import { Combobox, Transition } from '@headlessui/react';
 import { manufacturers } from '@/constants';
 import Image from 'next/image';
-import { isTemplateExpression } from 'typescript';
 
 const SearchManufacturer = ({ manufacturer, setManufacturer }: SearchManufacturerProps) => {
   const [query, setQuery] = useState('');
@@ -14,12 +13,12 @@ const SearchManufacturer = ({ manufacturer, setManufacturer }: SearchManufacture
     : manufacturers.filter((item) => (
       item.toLowerCase()
         .replace(/\s+/g, "")
-        .includes(query.toLowerCase().replace(/\s+/g, ""))
-    ))
+        .includes(query.toLowerCase().replace(/\s+/g, "")
+        )))
 
   return (
     <div className="search-manufacturer">
-      <Combobox>
+      <Combobox value={manufacturer} onChange={setManufacturer}>
         <div className="relative w-full">
           <Combobox.Button className="absolute top-[14px]">
             <Image
@@ -44,7 +43,31 @@ const SearchManufacturer = ({ manufacturer, setManufacturer }: SearchManufacture
             afterLeave={() => setQuery('')}
           >
             <Combobox.Options>
-
+              {filteredManufacturers.map((item) => (
+                <Combobox.Option
+                  key={item}
+                  className={({ active }) => `
+                    relative search-manufacturer__option ${active ? 'bg-primary-blue text-white' : 'text-gray-900'}
+                    `}
+                  value={item}
+                >
+                  {({ selected, active }) => (
+                    <>
+                      <span className={`block truncate ${selected ? 'font-medium' : 'font-normal'}`}
+                      >
+                        {item}
+                      </span>
+                      {selected ? (
+                        <span
+                          className={`absolute inset-y-0 left-0 flex items-center pl-3 ${active ? 'text-white' : 'text-teal-600'}`}
+                        >
+                        </span>
+                      ) : null}
+                    </>
+                  )}
+                </Combobox.Option>
+              )
+              )}
             </Combobox.Options>
 
           </Transition>
